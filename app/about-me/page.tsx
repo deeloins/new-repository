bafc1928ec8fcf5a54b696295035def4c0c2e04c
deeloins <114101485+deeloins@users.noreply.github.com>
2025-05-21 05:46:15 +0100

    Update page.tsx

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -17,18 +17,18 @@ import picture9 from "../images/mrLawal_(9).png";
 import picture10 from "../images/mrLawal_(10).png";
 import picture11 from "../images/mrLawal_(11).png";
 
-export function Slider() {
+function Slider() {
   const images = [
-    picture1,
-    picture2,
-    picture4,
-    picture5,
-    picture6,
-    picture7,
-    picture8,
-    picture9,
-    picture10,
-    picture11,
+    { src: picture1, width: 500, height: 500 },
+    { src: picture2, width: 500, height: 500 },
+    { src: picture4, width: 500, height: 500 },
+    { src: picture5, width: 500, height: 500 },
+    { src: picture6, width: 500, height: 500 },
+    { src: picture7, width: 500, height: 500 },
+    { src: picture8, width: 500, height: 500 },
+    { src: picture9, width: 500, height: 500 },
+    { src: picture10, width: 500, height: 500 },
+    { src: picture11, width: 500, height: 500 },
   ];
 
   const [index, setIndex] = useState(0);
@@ -42,120 +42,121 @@ export function Slider() {
   }, [images.length]);
 
   return (
-    <>
-      <div className="imageSlider">
-        <Image src={images[index]} className="image" alt="image" />
-      </div>
-    </>
+    <div className="imageSlider">
+      <Image
+        src={images[index].src}
+        width={images[index].width}
+        height={images[index].height}
+        className="image"
+        alt="Portfolio image"
+        priority
+      />
+    </div>
   );
 }
 
-export function Experience() {
+function Experience() {
   return (
-    <>
-      <div className="p-3.5 w-90 border-[0.1rem] border-[#3b3a3a] rounded-xl bg-[#1f1c1c] experienceSection">
-        <h2 className="text-left">Experience</h2>
+    <div className="p-3.5 w-90 border-[0.1rem] border-[#3b3a3a] rounded-xl bg-[#1f1c1c] experienceSection">
+      <h2 className="text-left">Experience</h2>
 
-        <section className="pt-5">
-          <div className="flex gap-10 pb-3 experience">
-            <div className="text-[#645d5d]">2024 - Now</div>
-            <div className="text-left">
-              <h2>Product Designer</h2>
-              <div className="flex text-[#645d5d]">
-                Stringify Consulting
-                <button className="remote">Remote</button>
-              </div>
+      <section className="pt-5">
+        <div className="flex gap-10 pb-3 experience">
+          <div className="text-[#645d5d]">2024 - Now</div>
+          <div className="text-left">
+            <h2>Product Designer</h2>
+            <div className="flex text-[#645d5d]">
+              Stringify Consulting
+              <button className="remote">Remote</button>
             </div>
           </div>
+        </div>
 
-          <div className="flex gap-10 pb-3 experience">
-            <div className="text-[#645d5d]">2023 - 2024</div>
-            <div className="text-left">
-              <h2>Intern Product Designer</h2>
-              <div className="flex text-[#645d5d] remotebtnflex">
-                Fiducia Data Service
-                <button className="remote">Hybrid</button>
-              </div>
+        <div className="flex gap-10 pb-3 experience">
+          <div className="text-[#645d5d]">2023 - 2024</div>
+          <div className="text-left">
+            <h2>Intern Product Designer</h2>
+            <div className="flex text-[#645d5d] remotebtnflex">
+              Fiducia Data Service
+              <button className="remote">Hybrid</button>
             </div>
           </div>
+        </div>
 
-          <div className="flex gap-10 pb-3 experience">
-            <div className="text-[#645d5d]">2022 - 2023</div>
-            <div className="text-left">
-              <h2>Intern Product Designer</h2>
-              <div className="flex text-[#645d5d]">
-                Handiworker Tech
-                <button className="remote">Remote</button>
-              </div>
+        <div className="flex gap-10 pb-3 experience">
+          <div className="text-[#645d5d]">2022 - 2023</div>
+          <div className="text-left">
+            <h2>Intern Product Designer</h2>
+            <div className="flex text-[#645d5d]">
+              Handiworker Tech
+              <button className="remote">Remote</button>
             </div>
           </div>
-        </section>
+        </div>
+      </section>
 
-        <hr className="h-0.5 mt-4 mb-4 divider" />
+      <hr className="h-0.5 mt-4 mb-4 divider" />
 
-        <section className="companyDiv">
-          <span className="text-[#645d5d] ">
-            Companies I&apos;ve collaborated with
-          </span>
-          <div className="flex gap-9 mt-3.5 companylogos">
-            <Image src={Lty} alt="logo" className="logo" />
-            <Image src={kawts} alt="logo" className="logo" />
-            <Image src={thenod} alt="logo" className="logo" />
-            <Image src={tantalizer} alt="logo" className="logo" />
-          </div>
-        </section>
-      </div>
-    </>
+      <section className="companyDiv">
+        <span className="text-[#645d5d]">
+          Companies I&apos;ve collaborated with
+        </span>
+        <div className="flex gap-9 mt-3.5 companylogos">
+          <Image src={Lty} alt="Lty logo" width={50} height={50} className="logo" />
+          <Image src={kawts} alt="Kawts logo" width={50} height={50} className="logo" />
+          <Image src={thenod} alt="Thenod logo" width={50} height={50} className="logo" />
+          <Image src={tantalizer} alt="Tantalizer logo" width={50} height={50} className="logo" />
+        </div>
+      </section>
+    </div>
   );
 }
 
-export function Education() {
+function Education() {
   return (
-    <>
-      <div className="p-5 w-90 border-[0.1rem] border-[#3b3a3a] rounded-xl bg-[#1f1c1c] experienceSection">
-        <h2 className="text-left pt-2.5">EDUCATION/CERTIFICATION</h2>
+    <div className="p-5 w-90 border-[0.1rem] border-[#3b3a3a] rounded-xl bg-[#1f1c1c] experienceSection">
+      <h2 className="text-left pt-2.5">EDUCATION/CERTIFICATION</h2>
 
-        <section className="pt-5">
-          <div className="flex gap-5 pb-3 education">
-            <div className="text-[#3b3a3a]">2016 - 2019</div>
-            <div className="text-left">
-              <h2>B.Sc (Ed)Computer Science</h2>
-              <div className="flex text-[#3b3a3a]">
-                University of Nigeria, Nsukka
-              </div>
+      <section className="pt-5">
+        <div className="flex gap-5 pb-3 education">
+          <div className="text-[#3b3a3a]">2016 - 2019</div>
+          <div className="text-left">
+            <h2>B.Sc (Ed)Computer Science</h2>
+            <div className="flex text-[#3b3a3a]">
+              University of Nigeria, Nsukka
             </div>
           </div>
+        </div>
 
-          <div className="flex gap-18 pb-3 education">
-            <div className="text-[#3b3a3a]">2024</div>
-            <div className="text-left">
-              <h2>
-                Figma UI UX Design <br /> Advanced
-              </h2>
-              <div className="flex text-[#3b3a3a]">
-                Udemy &bull; Daniel Walter Scott
-              </div>
+        <div className="flex gap-18 pb-3 education">
+          <div className="text-[#3b3a3a]">2024</div>
+          <div className="text-left">
+            <h2>
+              Figma UI UX Design <br /> Advanced
+            </h2>
+            <div className="flex text-[#3b3a3a]">
+              Udemy &bull; Daniel Walter Scott
             </div>
           </div>
+        </div>
 
-          <div className="flex gap-18 pb-3 education">
-            <div className="text-[#3b3a3a]">2024</div>
-            <div className="text-left">
-              <h2>User Experience Design Fundamentals</h2>
-              <div className="flex text-[#3b3a3a]">Udemy &bull; Joe Natoli</div>
-            </div>
+        <div className="flex gap-18 pb-3 education">
+          <div className="text-[#3b3a3a]">2024</div>
+          <div className="text-left">
+            <h2>User Experience Design Fundamentals</h2>
+            <div className="flex text-[#3b3a3a]">Udemy &bull; Joe Natoli</div>
           </div>
+        </div>
 
-          <div className="flex gap-18 pb-3 education">
-            <div className="text-[#3b3a3a]">2024</div>
-            <div className="text-left">
-              <h2>Google UX Design</h2>
-              <div className="flex text-[#3b3a3a]">Coursera</div>
-            </div>
+        <div className="flex gap-18 pb-3 education">
+          <div className="text-[#3b3a3a]">2024</div>
+          <div className="text-left">
+            <h2>Google UX Design</h2>
+            <div className="flex text-[#3b3a3a]">Coursera</div>
           </div>
-        </section>
-      </div>
-    </>
+        </div>
+      </section>
+    </div>
   );
 }
 
